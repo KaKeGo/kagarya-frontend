bdba771ee68c5a188efc4e427fa61b936e227b54
KaKeGo <funfcia@hotmail.com>
2023-05-14 20:52:32 +0200

    create csrf token

---
+++ src/compontents/CSRFToken/CSRFToken.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
+
+axios.defaults.xsrfHeaderName = 'x-csrftoken'
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.withCredentials = true
+
+
+const CSRFToken = () => {
+    const [csrftoken, setcsrftoken] = useState('');
+
+    const getCookie = (name) => {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            let cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                let cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                await axios.get(`${process.env.REACT_APP_API_URL}/account/csrftoken`)
+            } catch (err) {}
+        } 
+        fetchData()
+        setcsrftoken(getCookie('csrftoken'))
+    }, [])
+
+    return (
+        <input type="hidden" name="csrfmiddlewaretoken" value={csrftoken}/>
+    )
+}
+
+export default CSRFToken
