194f86b3dae7c877bab3712060fa00cae76c6be3
KaKeGo <funfcia@hotmail.com>
2023-05-14 19:45:12 +0200

    commit

--- src/store.js
+++ src/store.js
@@ -1,3 +1,15 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
+
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
