1bb38986974ff5ff7747a3a192bd6c05add76bbe
KaKeGo <funfcia@hotmail.com>
2023-05-14 20:15:21 +0200

    add reducer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
+import rootReducer from './reducers'
 
 
 const initialState = {}
@@ -8,6 +9,7 @@ const initialState = {}
 const middleware = [thunk]
 
 const store = createStore(
+    rootReducer,
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
 )
