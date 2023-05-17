316ef6ec2a88f4314876a230587fe9b53666a5f6
KaKeGo <funfcia@hotmail.com>
2023-05-17 22:38:22 +0200

    add fetch data from api

--- src/actions/Profile/ProfileList.jsx
+++ src/actions/Profile/ProfileList.jsx
@@ -1,7 +1,27 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 
-// const loadAllProfiles = createAsyncThunk('')
+export const loadAllProfiles = createAsyncThunk('profiles/list/', async (_, thunkApi) => {
+    try {
+        const res = await fetch('http://localhost:8000/account/list/', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+            }      
+        })
+
+        const data = await res.json()
+
+        if(res.status === 200) {
+            return data
+        } else {
+            return thunkApi.rejectWithValue(data)
+        }
+
+    } catch(err) {
+        return thunkApi.rejectWithValue(err.res.data)
+    }
+})
 
 const profileState = {
     name: 'profile',
--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import profileReducer from './actions/Profile'
+import profileReducer from './actions/Profile/ProfileList'
 
 
 export const store = configureStore({
     reducer: {
         profile: profileReducer,
-    }
+    },
+    devTools: process.env.NODE_ENV !== 'production'
 })
